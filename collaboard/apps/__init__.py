d122b961df87c93080a053bb6fa958b461abe88b
Jovani <jovani.badinga7@gmail.com>
2025-06-28 22:41:06 -0400

    Initial Commit
    The starting project structure has been created. The necessary apps
    have been made and their starting structure has been set up. All
    components have been connected (templates, static, database, etc...)
    The starting migrations have been ran and everything seems to be up and
    ready to be used. Now we can begin implementing.

---
+++ collaboard/apps/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package will be storing all apps used in this webapp.
+Think of it as the main engine for apps. Each app inside
+of this folder is one important component of the engine that
+helps it run properly.
+"""
---
+++ collaboard/apps/base/urls.py
@@ -0,0 +1,2 @@
+from django.contrib import admin  # noqa: F401
+from django.urls import path  # noqa: F401
