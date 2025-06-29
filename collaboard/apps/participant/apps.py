264c3b97e2d041e7e0ee29f86784ca501b14fa29
Jovani <jovani.badinga7@gmail.com>
2025-06-28 23:14:09 -0400

    Bug Fix!
    Fixed a minor bug. In each app in the apps directory. In their
    apps.py Config. The name was just appname. This is wrong. It was supposed
    to be apps.appname to match the app names in the installed apps list.
    It has been fixed.

--- collaboard/apps/participant/apps.py
+++ collaboard/apps/participant/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ParticipantConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "participant"
+    name = "apps.participant"
