044b2046da242fe981475a3c586a3cc9308bc211
Jovani <jovani.badinga7@gmail.com>
2025-06-30 20:07:57 -0400

    Added Two Fields To CustomUser
    These fields are the created_at and updated_at. They are both stored
    in the database as a timezone.now() value inside of a DateTimeField
    attribute. I also added a description inside the __init__.py of this
    app, describing it's purpose.

---
+++ collaboard/apps/base/__init__.py
@@ -0,0 +1,7 @@
+"""
+This app defines the base utilities that the
+project needs to function properly.
+This includes the CustomUser Model which is used
+as the Director Model. Handling authentication
+and authorization.
+"""
--- collaboard/apps/base/models.py
+++ collaboard/apps/base/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
     REQUIRED_FIELDS = ["first_name", "last_name", "email"]
