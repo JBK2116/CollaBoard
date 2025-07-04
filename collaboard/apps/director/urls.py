72a271ce875f1ca6f91b567c0a51a51b4246cb47
Jovani <jovani.badinga7@gmail.com>
2025-07-03 22:36:20 -0400

    Fixed Bug
    The meeting id name in the url wasnt the same one as in the view.
    Caused an error. It has been fixed.

--- collaboard/apps/director/urls.py
+++ collaboard/apps/director/urls.py
@@ -5,5 +5,5 @@ from apps.director import views
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("create-meeting/", views.create_meeting, name="create-meeting"),
-    path("edit-meeting/<int:id>/", views.edit_meeting, name="edit-meeting"),
+    path("edit-meeting/<int:meeting_id>/", views.edit_meeting, name="edit-meeting"),
 ]
