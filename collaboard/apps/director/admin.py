9714669c354dc3d3d4dd2b4cf3b95255e61da7db
Jovani <jovani.badinga7@gmail.com>
2025-07-04 15:06:07 -0400

    Meeting Views/Urls Set up
    The meeting table has been registered to the admin table.
    Additionally I have created urls and starting views for more meeting
    functions. This includes delete meeting and my meetings. Additionally
    I have changed the url parameter in edit meeting to a string.
    The str will represent the uuid of the meeting object. It can easily
    be converted back into a uuid once received.

--- collaboard/apps/director/admin.py
+++ collaboard/apps/director/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from apps.director.models import Meeting
+
 # Register your models here.
+admin.site.register(Meeting)
--- collaboard/apps/director/views.py
+++ collaboard/apps/director/views.py
@@ -10,14 +10,32 @@ def dashboard(request):
 
 @login_required
 def create_meeting(request):
+    # More logic will be implemented later
     if request.method == "POST":
         print(request.POST)
     context = {}
     return render(request, "director/create_meeting.html", context)
 
+@login_required
 def edit_meeting(request, meeting_id):
+    # More logic will be implemented later
     context = {}
     if request.method == "POST":
         print(request.POST)
     print(meeting_id)
     return render(request, "director/edit_meeting.html", context)
+
+@login_required
+def delete_meeting(request, meeting_id):
+    # More logic will be implemented later
+    context = {}
+    if request.method == "POST":
+        print(request.POST)
+    print(meeting_id)
+    return render(request, "director/my_meetings.html", context)
+
+@login_required
+def my_meetings(request):
+    # More logic will be implemented later
+    context = {}
+    return render(request, "director/my_meetings.html", context)
