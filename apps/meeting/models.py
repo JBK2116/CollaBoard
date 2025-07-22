ac411641b4d6d39f44144cdcf368de1ebdeb8bf6
jovkb <jovani.badinga7@gmail.com>
2025-07-21 22:34:04 -0400

    Created Session models
    These session models will be used to improve the meeting connection.
    It will be used to track the current meeting state at all moments.

--- apps/meeting/models.py
+++ apps/meeting/models.py
@@ -1,11 +1,43 @@
 import uuid
 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from apps.director.models import Meeting, Question
 
 
 # Create your models here.
+class MeetingSession(models.Model):
+    id = models.IntegerField(primary_key=True)
+    meeting = models.OneToOneField(to=Meeting, on_delete=models.CASCADE)
+    current_duration = models.IntegerField(
+        null=False,
+        blank=False,
+        validators=[MinValueValidator(0), MaxValueValidator(3600)],
+        help_text="Duration in seconds",
+    )
+    current_question_index = models.IntegerField(
+        null=False,
+        blank=False,
+        validators=[MinValueValidator(0), MaxValueValidator(20)],
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ParticipantSession(models.Model):
+    id = models.IntegerField(primary_key=True)
+    meeting = models.ForeignKey(to=Meeting, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30, unique=True)
+    sessiontoken = models.CharField(
+        null=False,
+        max_length=66,
+        help_text="Used to identify previous meeting Session by the user",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Response(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     # Relationship Constraints
