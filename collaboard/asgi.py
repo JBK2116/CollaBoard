24b30e0e8d1d0b6745f5e2bbbf5dca0ad3a6931f
jovkb <jovani.badinga7@gmail.com>
2025-08-12 14:28:00 -0400

    Attemp 1 to fix circular import issue in asgi.py

--- collaboard/asgi.py
+++ collaboard/asgi.py
@@ -15,6 +15,8 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaboard.settings')
+django_asgi_app = get_asgi_application()
+
 # Import your WebSocket routing
 from apps.meeting.routing import websocket_urlpatterns
 
